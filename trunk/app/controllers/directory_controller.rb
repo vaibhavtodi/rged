472f666ada45fbce742ef5106a5af2728b116cdc
lesotdudestin <lesotdudestin@db5e94d3-6b3b-0410-8c26-9d47377895a7>
2007-10-02 16:10:57 +0000

    Actions list and get done

--- trunk/app/controllers/directory_controller.rb
+++ trunk/app/controllers/directory_controller.rb
@@ -1,13 +1,81 @@
 # Directory method
 class DirectoryController < ApplicationController
   def list
-    return_data = Hash.new()      
-    dir = (params[:dir] || 'dir')
-    return_data[:FilesCount] = 3      
-    return_data[:Files] = [{:name => dir, :size => 1254654, :lastChange => '2007-03-03'},
-                            {:name => 'titi.txt', :size => 1023, :lastChange => '2007-10-23'},
-                            {:name => 'tutu.txt', :size => 45675688600, :lastChange => '2007-05-01'}
-    ]
+    return_data = Hash.new()
+    #dir = (params[:dir] || 'dir')
+    dir = "/Users/papywarrior/ProjsPerso/lesotdudestinRubyDev/rged"
+
+    if dir == nil || dir == "" then
+      dir = "."
+    end
+
+    if File.exist?(dir) then
+      i = 0
+      return_data[:Files] = Array.new
+      if File.directory?(dir) then
+        d = Dir.entries(dir)
+        d.each{
+          |filename|
+          if filename != "." && filename != ".." then
+            return_data[:Files][i] = {:name => filename, :size => File.size(filename), :lastChange => File.atime(filename).asctime}
+            i = i + 1
+          end
+        }
+      end
+    end
+
+    if (return_data[:Files] != nil) then
+      return_data[:FilesCount] = return_data[:Files].length
+    else
+      return_data[:FilesCount] = 0
+    end
     render :text=>return_data.to_json, :layout=>false
   end
+
+  def get
+    #    dir = "/Users/papywarrior/ProjsPerso/lesotdudestinRubyDev/rged/"
+    dir = (params[:path] || 'path')
+    if dir == nil || dir == "" then
+      dir = "."
+    end
+
+    if File.exist?(dir) then
+      i = 0
+      return_data = Array.new
+      if File.directory?(dir) then
+        d = Dir.entries(dir)
+        d.each{
+          |filename|
+          if File.readable?(filename) && File.executable?(filename) == false && File.writable?(filename) == false then
+            readonly = true
+          end
+          if filename != "." && filename != ".." then
+            if File.directory?(filename) then
+              if readonly then
+                return_data[i] = {:text => filename, :cls => "folder", :disabled => true, :leaf => false}
+              else
+                return_data[i] = {:text => filename, :cls => "folder", :disabled => false, :leaf => false}
+              end
+
+            else
+              if readonly then
+                return_data[i] = {:text => filename, :cls => File.extname(filename).sub(".", 'file-'), :disabled => false, :leaf => true}
+              else
+                return_data[i] = {:text => filename, :cls => File.extname(filename).sub(".", 'file-'), :disabled => false, :leaf => true}
+              end
+            end
+            i = i + 1
+          end
+        }
+      end
+    end
+    render :text=>return_data.to_json, :layout=>false
+  end
+
+#  def rename
+#    newname = dir = (params[:newname] || 'newname')
+
+
+#  end
+
 end
