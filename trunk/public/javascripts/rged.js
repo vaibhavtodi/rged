ca405ca30b895486a315b96eec318517fc5f632b
patou.de.saint.steban <patou.de.saint.steban@db5e94d3-6b3b-0410-8c26-9d47377895a7>
2007-10-03 09:23:01 +0000

    Correct error in the directory controller

--- trunk/public/javascripts/rged.js
+++ trunk/public/javascripts/rged.js
@@ -11,25 +11,34 @@ function getIframeDocument(el) {
 Ext.BLANK_IMAGE_URL = '/javascripts/extjs/resources/images/default/s.gif';
 
 var Rged= function() {
-    var northPanel, southPanel, eastPanel, westPanel, centerPanel;
+    // All Panel
+    var northPanel, westPanel, centerPanel;
+    var menu;
+    var tree;
+    var grid;
+    var textBox;
+    
     return {
         init : function() {
            Ext.QuickTips.init();
            
            // tree in the panel
-	var tree = new Ext.ux.FileTreePanel('tree', {
+	tree = new Ext.ux.FileTreePanel('tree', {
 		animate: true
-//		, dataUrl: 'filetree.php'
+		, dataUrl: '/directory/get'
+                , renameUrl: '/directory/rename'
+                , deleteUrl: '/directory/delete'
+                , newdirUrl: '/directory/newdir'
+                , iconPath: '../images/icons/'
 		, readOnly: false
 		, containerScroll: true
 		, enableDD: true
-		, enableUpload: true
+		, enableUpload: false
 		, enableRename: true
 		, enableDelete: true
 		, enableNewDir: true
 		, uploadPosition: 'menu'
-//		, edit: true
-//		, sort: true
+		, edit: true
 		, maxFileSize: 1048575
 		, hrefPrefix: '/filetree/'
 		, pgCfg: {
@@ -47,68 +56,72 @@ var Rged= function() {
 	});
 	
 	// {{{
-	var root = new Ext.tree.TreeNode({text:'/', path:'root', allowDrag:false});
+	var root = new Ext.tree.AsyncTreeNode({text:'/', path:'/', allowDrag:false});
 	tree.setRootNode(root);
 	tree.render();
+        tree.on('click', function(node, elt) {textBox.setValue(tree.getPath(node))});
 	root.expand();
 //	tree.on('beforerename', function(tree, node, oldname, newname) {debugger;return false});
 //	tree.setReadOnly(true);
 //	tree.setReadOnly(false);
 	// }}}
             
-           var menu= new Ext.Toolbar('menu');
-           menu.addButton({
-               text: 'Rename', cls: 'x-btn-text-icon scroll-bottom', handler: function(o, e) {
-                   var iframeDoc = getIframeDocument('center-iframe');
-                   iframeDoc.body.scrollTop = iframeDoc.body.scrollHeight;
-               }
-           });
-           menu.addButton({
-               text: 'Delete', cls: 'x-btn-text-icon scroll-top', handler: function(o, e) {
-                   var iframeDoc = getIframeDocument('center-iframe');
-                   iframeDoc.body.scrollTop = 0;
-               }
-           });
-           var mainLayout = new Ext.BorderLayout(document.body, {
-                north: {
-                    split:false,
-                    initialSize: 32,
-                    titlebar: false
-                },
-                west: {
-                    split:true,
-                    initialSize: 250,
-                    minSize: 175,
-                    maxSize: 400,
-                    titlebar: true,
-                    collapsible: true,
-                    animate: true,
-                    useShim:true,
-                    cmargins: {top:2,bottom:2,right:2,left:2}
-                },
-                center: {
-                    titlebar: true,
-                    title: 'Files',
-                    autoScroll:false,
-                    tabPosition: 'top',
-                    closeOnTab: true,
-                    //alwaysShowTabs: true,
-                    resizeTabs: true
-                }
-            });
-            mainLayout.beginUpdate();
-            mainLayout.add('north', northPanel = new Ext.ContentPanel('menu', { 
-                fitToFrame: true, closable: false 
-            }));
-            mainLayout.add('west', westPanel = new Ext.ContentPanel('tree', { 
-                fitToFrame: true, closable: false, title: 'Folders'
-            }));
-            mainLayout.add('center', centerPanel = new Ext.ContentPanel('grid', { 
-                fitToFrame: true, autoScroll: true, resizeEl: grid, title: 'Files'
-            })); 
-            mainLayout.endUpdate();
-            //northPanel.setContent('This panel will be used for a header');
+       menu = new Ext.Toolbar('menu');
+       menu.addButton({
+           text: 'Rename', cls: 'x-btn-text-icon scroll-bottom', handler: function(o, e) {
+               var iframeDoc = getIframeDocument('center-iframe');
+               iframeDoc.body.scrollTop = iframeDoc.body.scrollHeight;
+           }
+       });
+       menu.addButton({
+           text: 'Delete', cls: 'x-btn-text-icon scroll-top', handler: function(o, e) {
+               var iframeDoc = getIframeDocument('center-iframe');
+               iframeDoc.body.scrollTop = 0;
+           }
+       });
+       textBox = new Ext.form.TextField ();
+       textBox.on('change', function(field, newval, oldval) {ds.load ({params: {dir : newval}});});
+       menu.addField(textBox);
+       var mainLayout = new Ext.BorderLayout(document.body, {
+            north: {
+                split:false,
+                initialSize: 32,
+                titlebar: false
+            },
+            west: {
+                split:true,
+                initialSize: 250,
+                minSize: 175,
+                maxSize: 400,
+                titlebar: true,
+                collapsible: true,
+                animate: true,
+                useShim:true,
+                cmargins: {top:2,bottom:2,right:2,left:2}
+            },
+            center: {
+                titlebar: true,
+                title: 'Files',
+                autoScroll:false,
+                tabPosition: 'top',
+                closeOnTab: true,
+                //alwaysShowTabs: true,
+                resizeTabs: true
+            }
+        });
+        mainLayout.beginUpdate();
+        mainLayout.add('north', northPanel = new Ext.ContentPanel('menu', { 
+            fitToFrame: true, closable: false 
+        }));
+        mainLayout.add('west', westPanel = new Ext.ContentPanel('tree', { 
+            fitToFrame: true, closable: false, title: 'Folders'
+        }));
+        mainLayout.add('center', centerPanel = new Ext.ContentPanel('grid', { 
+            fitToFrame: true, autoScroll: true, resizeEl: grid, title: 'Files'
+        })); 
+        mainLayout.endUpdate();
        
+        // Grid Data Store
         ds = new Ext.data.Store({
             proxy: new Ext.data.HttpProxy({url: '/directory/list'}),
             reader: new Ext.data.JsonReader({
@@ -121,7 +134,8 @@ var Rged= function() {
                 {name: 'lastChange', mapping: 'lastChange', type: 'date', dateFormat: 'Y-m-d'}
             ])
         });
-		// example of custom renderer function
+	
+        // Convert a file size in Kilo/Mega/Giga Octet
         function size(val){
             if(val < 1024){
                 return Math.round( val) + ' o';
@@ -142,17 +156,18 @@ var Rged= function() {
             }
             return val;
         }
-		// the DefaultColumnModel expects this blob to define columns. It can be extended to provide
-        // custom or reusable ColumnModels
+	// Column Model of the grid
         var colModel = new Ext.grid.ColumnModel([
-			{id:'name',header: "Name", width: 160, sortable: true, locked:false, dataIndex: 'name'},
+			{id:'name',header: "Name", width: 160, sortable: true, locked:false, dataIndex: 'name'
+                            , editor: new Ext.grid.GridEditor(new Ext.form.TextField({
+                                allowBlank: false}))},
 			{header: "Size", width: 75, sortable: true, renderer: size, dataIndex: 'size'},
 			{header: "Last Updated", width: 85, sortable: true, renderer: Ext.util.Format.dateRenderer('m/d/Y'), dataIndex: 'lastChange'}
 		]);
 
 
         // create the Grid
-        var grid = new Ext.grid.Grid('grid', {
+        var grid = new Ext.grid.EditorGrid('grid', {
             ds: ds,
             cm: colModel,
             enableDragDrop : true,
@@ -160,10 +175,9 @@ var Rged= function() {
         });
         
         grid.render();
-        ds.load ({params: {dir : '/home/patou'}});
+        ds.load ({params: {dir : './'}});
 
-        grid.getSelectionModel().selectFirstRow();
-        Ext.get('center-iframe').dom.src = 'http://www.google.fr';
+        //grid.getSelectionModel().selectFirstRow();
         }
     };
 }();
