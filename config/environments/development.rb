a34ed2d80a7c68e220c6b04761b4f8144253393f
lesotdudestin <lesotdudestin@gmail.com>
2007-11-14 17:05:39 +0000

    folder config update for Rails 2.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,19 +3,27 @@
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
+puts "1"
 config.cache_classes = false
 
 # Log error messages when you accidentally call methods on nil.
+puts "2"
 config.whiny_nils = true
 
 # Enable the breakpoint server that script/breakpointer connects to
 # config.breakpoint_server = true
 
 # Show full error reports and disable caching
+puts "3"
 config.action_controller.consider_all_requests_local = true
+puts "4"
 config.action_controller.perform_caching             = false
+puts "5"
 config.action_view.cache_template_extensions         = false
+puts "6"
 config.action_view.debug_rjs                         = true
 
 # Don't care if the mailer can't send
+puts "7"
 config.action_mailer.raise_delivery_errors = false
+puts "8"
--- config/war.rb
+++ config/war.rb
@@ -5,6 +5,10 @@
 #maven_library 'org.jruby.extras', 'goldspike', '1.3-SNAPSHOT'
 
 # Add a Java library from the Maven repository:
+puts "war 1"
 maven_library 'mysql', 'mysql-connector-java', '5.0.4'
+puts "war 2"
 add_gem 'gettext'
+puts "war 3"
 add_gem 'rubyzip'
+puts "war 4"
