bef66641b3c91c407724f67ee68f8ec66e08a782
lesotdudestin <lesotdudestin@gmail.com>
2007-11-15 17:02:55 +0000

    some modification for Rails 2.0 with ruby and Jruby

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -52,6 +52,6 @@ class ApplicationController < ActionController::Base
   session :session_key => '_rged_session_id'
   include AuthenticatedSystem
   before_filter :login_from_cookie
-  
-  
+
+
 end
