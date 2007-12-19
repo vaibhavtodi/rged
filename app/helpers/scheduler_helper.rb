140105cb2b34cf06204114bc1b05f7a2fa7e2d09
lesotdudestin <lesotdudestin@gmail.com>
2007-12-19 09:02:00 +0000

    scheduler updated

--- app/helpers/scheduler_helper.rb
+++ app/helpers/scheduler_helper.rb
@@ -1,2 +1,74 @@
 module SchedulerHelper
+  
+  attr_reader :day 
+
+  
+  @@day = "['Any'], 
+      ['Monday'], 
+      ['Thuesday'], 
+      ['Wednesday'], 
+      ['Thursday'], 
+      ['Friday'], 
+      ['Saturday'], 
+      ['Sunday'] 
+    "  
+  @@week = "['1'],
+            ['2'],
+            ['3'],
+	    ['4'],
+            ['5']"
+  
+  @@month =   "['January'],
+        ['February'],
+	['March'],
+	['April'],
+	['May'],
+	['June'],
+	['July'],
+	['August'],
+	['September'],
+	['October'],
+	['November'],
+	['December']"
+  
+  
+  def get_checkbox(id, name, label)
+    "xtype:'checkbox',
+    boxLabel:'#{label}',
+    name:'#{name}',
+    inputValue:'cbvalue',
+    id:'#{id}'"
+  end
+  
+  def get_combox(id, editable, empty, data)
+    tmp = ""
+    case data
+    when "day"
+      tmp = @@day
+    when "week"
+      tmp = @@week
+    when "month"
+      tmp = @@month
+    end
+
+    "     xtype: 'combo',
+          id:'#{id}',
+          editable:#{editable},
+          emptyText:'#{empty}',
+          displayField:'field',
+          typeAhead: true,
+          mode: 'local',
+          triggerAction: 'all',
+	  store:new Ext.data.SimpleStore({
+            fields: ['field'],
+            data:[
+                  #{tmp}
+                  ]
+          }),
+	  selectOnFocus:true,
+	  x:120,
+	  y:-15   
+      " 
+  end
+  
 end
