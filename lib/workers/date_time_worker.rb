68f55b3f1e2dd8fba766c5780571dbdeea77393c
lesotdudestin <lesotdudestin@db5e94d3-6b3b-0410-8c26-9d47377895a7>
2007-12-27 19:39:24 +0000

    scheduler code cleaned

---
+++ lib/workers/date_time_worker.rb
@@ -0,0 +1,26 @@
+# Put your code that runs your task inside the do_work method it will be
+# run automatically in a thread. You have access to all of your rails
+# models.  You also get logger and results method inside of this class
+# by default.
+class DateTimeWorker < BackgrounDRb::MetaWorker#::RailsBase
+  set_worker_name :date_time_worker
+  def create(args = nil)
+    # This method is called in it's own new thread when you
+    # call new worker. args is set to :args
+    #logger.info("\033[33m Do Work  #{Time.now.to_s} \033[m")
+    @counter = 0
+    add_periodic_timer(2) { increment_counter }
+    #register_status("Running")
+  end
+  
+  def increment_counter
+     @counter += 1
+     register_status(@counter)
+  end
+  
+  def set_log
+    logger.info("\033[33m Do Work  #{Time.now.to_s} \033[m")
+  end
+
+end
+#DateWorker.register
